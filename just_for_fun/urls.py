0d05e5f43d1ea9d3c1a09807c26df9afa7848128
ebuitragod <ebuitragod@gmail.com>
2020-04-29 15:12:11 -0500

    API endpoint que no funciona

--- just_for_fun/urls.py
+++ just_for_fun/urls.py
@@ -15,7 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework import routers
+from users import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+#Wire up our API using an automatic URL routing.
+#Additionally, we include login URLs for the browsable API. 
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
 ]
+urlpatterns += router.urls
